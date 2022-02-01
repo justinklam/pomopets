67d61799c56ffbc73fbe4f8fac111af405198913
Justin Lam <justin.kh.lam@gmail.com>
2022-02-01 05:08:10 +0000

    set up app.js for sequalize and dotenv. set up Sequalize to read from .env for user/pass/db. correct file pathing and error messages

--- pomo-api/app.js
+++ pomo-api/app.js
@@ -3,11 +3,18 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+require('dotenv').config()
+const { Sequelize } = require('sequelize');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: 'localhost',
+  dialect: 'postgres'
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,11 +38,14 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
+  console.log(err.message);
+
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  // res.render('error');
+  res.send('error');
 });
 
 module.exports = app;
