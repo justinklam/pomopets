a8c6ad088057f8030e9d4a3fbf563cb6bf4d4ce1
Justin Lam <justin.kh.lam@gmail.com>
2022-02-01 05:14:50 +0000

    correct path routing. add simple test query to ensure it's functioning

--- pomo-api/routes/index.js
+++ pomo-api/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const { User } = require('../db/models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  // Test - Find Users
+  // const findUsers = User.findAll();
+  // findUsers.then(result => {
+  //   console.log('RESULT', result)
+  // })
+  
+  // console.log("USERS", findUsers);
+  
+  res.send({ title: 'Express' });
 });
 
 module.exports = router;
