a673f72f240bd6b5d2bd5ab8402c37f6bd2778fa
Justin Lam <justin.kh.lam@gmail.com>
2022-02-06 00:23:06 -0500

    add in logic for login route

--- pomo-api/routes/login.js
+++ pomo-api/routes/login.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
-router.post('login', function(req, res) {
-
-  const user = User.findOne({
-    where: { email: req.body.email}
+router.post('login', async function(req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+  const userID = await User.findOne({
+    where: { email: email}
   });
-  if (!user || bcrypt.compareSync(password, user.password)) {
+
+  if (!email || !password) {
+    res.send('A field is empty!')    
+  };
+
+  if (!userID || bcrypt.compareSync(password, userID.password)) {
     res.send('Account Credentials are incorrect!')    
-  }
+  };
+
+  // user_id key, newUser.id is the value of the cookie
+  res.cookie('user_id', newUser.id, {
+    maxAge: 900000, httpOnly: true
+  });
+  res.send('Successful');
 
 });
 
