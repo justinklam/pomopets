8002660653ebee8b7d2d3f9aa3753521aa94232c
Justin Lam <justin.kh.lam@gmail.com>
2022-02-05 21:13:25 -0500

    add in require for bcrypt

--- pomo-api/app.js
+++ pomo-api/app.js
@@ -2,6 +2,7 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const logger = require('morgan');
 require('dotenv').config()
 const { Sequelize } = require('sequelize');
