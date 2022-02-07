480ee0b10b974bef729e1687b37f6c965dc25219
Justin Lam <justin.kh.lam@gmail.com>
2022-02-06 19:25:25 -0500

    change table names to lower case to resolve psql errors

--- pomo-api/db/migrations/20220206064351-create-user.js
+++ pomo-api/db/migrations/20220206064351-create-user.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('users', {
       id: {
         allowNull: false,
         autoIncrement: true,
--- pomo-api/db/migrations/20220206064432-create-pet.js
+++ pomo-api/db/migrations/20220206064432-create-pet.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Pets', {
+    await queryInterface.createTable('pets', {
       id: {
         allowNull: false,
         autoIncrement: true,
