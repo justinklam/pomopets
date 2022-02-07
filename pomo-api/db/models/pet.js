1956e02b2d23b9d7e90330ec3b5317fcee529cc5
Rhea <Rheaazarrag@gmail.com>
2022-02-06 19:38:43 -0800

    Comment line 13 to test

--- pomo-api/db/models/pet.js
+++ pomo-api/db/models/pet.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Pet.belongsToMany(models.User, { through: models.users_pets });
+      //  Pet.belongsToMany(models.User, { through: models.users_pets });
     }
   }
   Pet.init({
