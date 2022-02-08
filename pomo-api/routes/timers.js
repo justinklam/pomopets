3c44341d06db11345f0ba7b3437a6722d5173d0e
Justin Lam <justin.kh.lam@gmail.com>
2022-02-08 16:18:29 -0500

    add in additional test logic for timers.js route

--- pomo-api/routes/timers.js
+++ pomo-api/routes/timers.js
@@ -2,17 +2,22 @@ const express = require('express');
 const router = express.Router();
 const { User, Timer } = require('../db/models');
 
-router.get('/new_timer', async function(req, res, next) {
+
+router.get('/', async function(req, res) {
+  res.render('timer');
+});
+
+router.post('/', async function(req, res, next) {
 
   const new_timer = await Timer.create({
-    user_id: req.body.User.id,
+    user_id: User.id,
     description: 'Example',
     is_active: true
   });
 
   console.log('newTimer-----', new_timer)
   
-  res.send({ title: 'timer' });
+  res.send('Timer created');
 });
 
 module.exports = router;
