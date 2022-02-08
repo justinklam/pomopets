7eaaea71bd0c3e6c9f1bfb4aa0ae5a078aca94a9
Justin Lam <justin.kh.lam@gmail.com>
2022-02-07 23:52:49 -0500

    correct error in logic, add in return statements to end route when unsucessful

--- pomo-api/routes/login.js
+++ pomo-api/routes/login.js
@@ -13,23 +13,25 @@ router.post('/', async function(req, res) {
   const password = req.body.password;
 
   const userID = await User.findOne({
-    where: { email: email}
+    where: { 
+      email: email
+    }
   });
-  console.log('user', userID)
+  console.log('user-----', userID)
 
   if (!email || !password) {
-    res.send('A field is empty!')    
+    return res.send('A field is empty!')  
   };
 
-  if (!userID || bcrypt.compareSync(password, userID.password)) {
-    res.send('Account Credentials are incorrect!')    
+  if (!userID || !bcrypt.compareSync(password, userID.password)) {
+    return res.send('Account Credentials are incorrect!')    
   };
 
   // user_id key, newUser.id is the value of the cookie
   res.cookie('user_id', User.id, {
     maxAge: 900000, httpOnly: true
   });
-  res.send('Successful');
+  res.send('Successful login');
 
 });
 
