f6ba2b696287f34f5fd6d35c0918e5da2653b56a
Justin Lam <justin.kh.lam@gmail.com>
2022-02-10 00:47:48 -0500

    add in previous routes from pomo-api-sequelize

---
+++ pomo-server/routes/timers.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+// const { User, Timer } = require('../db/models');
+
+router.get('/', async function(req, res) {
+  res.render('timer');
+});
+
+router.post('/', async function(req, res) {
+  // console.log('user.id----', User.id);
+
+  // const new_timer = await Timer.create({
+  //   UserId: 1,
+  //   description: 'Example',
+  //   is_active: true
+  // });
+
+  // console.log('new_timer-----', new_timer)
+  
+  res.send('Timer created');
+});
+
+module.exports = router;
