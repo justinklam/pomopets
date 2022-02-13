6fe96c86a082970f82dcba47d47256b96edcd3dd
Rhea <Rheaazarrag@gmail.com>
2022-02-13 15:40:45 -0800

    add a basic break message and timer class

--- pomo-react/src/Pomodoro.js
+++ pomo-react/src/Pomodoro.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Pomodoro() {
-  return <div className="pomodoro"></div>
+  return <div className="pomodoro">
+    <div className="message">
+      <div>Time for a break! New session starts in:</div>
+    </div>
+    <div className="timer">25:00</div>
+  </div>
 }
