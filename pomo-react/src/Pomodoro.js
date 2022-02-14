b8a24e3b8e4ac51cda779d80bee08c734c27c97c
Rhea <Rheaazarrag@gmail.com>
2022-02-13 16:16:17 -0800

    add logic to display proper timer format

--- pomo-react/src/Pomodoro.js
+++ pomo-react/src/Pomodoro.js
@@ -6,15 +6,20 @@ export default function Pomodoro() {
   const [ seconds, setSeconds ] = useState(0);
   const [ displayMessage, setDisplayMessage ] = useState(false);
 
-  useEffect(() => {
+  useEffect(() => { }, [ seconds ]) // side-effect every time seconds are updated
 
-  }, [ seconds ]) // side-effect every time seconds are updated
+  // ensure double digits in timer are always present
+  const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
 
-  return (<div className="pomodoro">
-    <div className="message">
-      <div>Time for a break! New session starts in:</div>
+  return (
+    <div className="pomodoro">
+      <div className="message">
+        {displayMessage && <div>Time for a break! Your next session starts in: </div>}
+        <div className="timer">
+          {timerMinutes}:{timerSeconds}
+        </div>
+      </div>
     </div>
-    <div className="timer">25:00</div>
-  </div>
   );
 }
