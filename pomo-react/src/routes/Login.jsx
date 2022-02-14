049f8c0f3f71fcdd6ef9711aec89f84acde1cb32
Justin Lam <justin.kh.lam@gmail.com>
2022-02-14 17:18:37 -0500

    add in Logic route useState, handleSubmit and handleChange handlers

--- pomo-react/src/routes/Login.jsx
+++ pomo-react/src/routes/Login.jsx
@@ -1,26 +1,36 @@
-// import { login } from "../helpers/helpers"
+import {useState} from "react";
+import { login } from "../helpers/helpers"
 
 export default function Login() {
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(user);
+  };
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUser({...user, [name]: value})
+  };
+
   return (
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Login</title>
-    </head>
     <body>
         <main>
           <h2>Login</h2>
           <form action="/login" method="post">
           <label>Email:
-              <input type="text" name="email"></input>
+              <input type="text" name="email" value={user.email} onChange={handleChange}/>
             </label>
             <label>Password:
-              <input type="password" name="password"></input></label>
-            <button type="submit">Login</button>
+              <input type="password" name="password" value={user.password} onChange={handleChange}/>
+            </label>
+            <button type="submit" onClick={handleSubmit}>Login</button>
           </form>
         </main>
     </body>
-    </html>
 )};
