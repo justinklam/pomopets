7e645b680e0a196469dc2ae276a57274bb2accde
Rhea <Rheaazarrag@gmail.com>
2022-02-13 16:04:22 -0800

    assign useState values, call useEffect

--- pomo-react/src/Pomodoro.js
+++ pomo-react/src/Pomodoro.js
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from "react";
 
 export default function Pomodoro() {
-  return <div className="pomodoro">
+
+  const [ minutes, setMinutes ] = useState(25);
+  const [ seconds, setSeconds ] = useState(0);
+  const [ displayMessage, setDisplayMessage ] = useState(false);
+
+  useEffect(() => {
+
+  }, [ seconds ]) // side-effect every time seconds are updated
+
+  return (<div className="pomodoro">
     <div className="message">
       <div>Time for a break! New session starts in:</div>
     </div>
     <div className="timer">25:00</div>
   </div>
+  );
 }
