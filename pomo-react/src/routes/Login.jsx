9080bcc35718d6e73865ab246e80106b8b3e3c59
Justin Lam <justin.kh.lam@gmail.com>
2022-02-14 23:15:30 -0500

    convert login form logic to React-boostrap components

--- pomo-react/src/routes/Login.jsx
+++ pomo-react/src/routes/Login.jsx
@@ -1,5 +1,7 @@
 import {useState} from "react";
 import { login } from "../helpers/helpers"
+import { Form, Button } from 'react-bootstrap';
+
 
 export default function Login() {
   const [user, setUser] = useState({
@@ -21,14 +23,25 @@ export default function Login() {
   return (
     <main>
       <h2>Login</h2>
-      <form action="/login" method="post">
-      <label>Email:
-          <input type="text" name="email" value={user.email} onChange={handleChange}/>
-        </label>
-        <label>Password:
-          <input type="password" name="password" value={user.password} onChange={handleChange}/>
-        </label>
-        <button type="submit" onClick={handleSubmit}>Login</button>
-      </form>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" name="email" value={user.email} onChange={handleChange}/>
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" name="password" value={user.password} onChange={handleChange}/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+          <Button variant="primary" type="submit" onClick={handleSubmit}>
+            Login
+          </Button>
+      </Form>
     </main>
 )};
