395519adcf2e2b3497b1bdc16a37d5d2917d10d7
Rhea <rheaazarraga@gmail.com>
2022-02-14 18:00:12 -0800

    add blurb explaining pomodoro technique and purpose of pomopets

--- pomo-react/src/components/Main.jsx
+++ pomo-react/src/components/Main.jsx
@@ -10,6 +10,14 @@ export default function Main(props) {
           <h4 className="subheader">
             This is {props.title}
           </h4>
+          <br/>
+          <h3>
+            <p> The Pomodoro Technique is a time management method that can be used for any task. For many people, time is an enemy. The anxiety triggered by “the ticking clock”, especially when it involves a deadline, leads to ineffective work and study habits which in turn lead to procrastination.</p>
+            
+            <p>The Pomodoro Technique aims to use time as a valuable ally in accomplishing what we want to do in the way we want to do it, and to enable us to continually improve the way we work or study.</p>
+
+            <p>PomoPets is an incentivized Pomodoro timer. Begin your focus session with a mysterious, hatchable egg and upon completing your focus session, you are rewarded with a cute digital pet! </p>
+          </h3>
         </div>
         <img src={egg} className="egg" width="400px" height="400px"/>
       </div>
