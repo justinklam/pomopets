965ca90f926e1d0c88740bf9481a56bf32f236af
Rhea <rheaazarraga@gmail.com>
2022-02-15 17:45:00 -0800

    add wide pets selection

--- pomo-react/src/routes/Pets.jsx
+++ pomo-react/src/routes/Pets.jsx
@@ -7,14 +7,17 @@ export default function Pets() {
             <h1 className="logo-text text-center"> Choose your PomoPet! </h1>
             <div className="pet-pics text-center"> 
               <img src={require('../assets/pets/cat1.gif')} width="185px" height="185px"/> 
-              <img src={require('../assets/pets/cat2.gif')} width="185px" height="185px"/> 
+              <img src={require('../assets/pets/cat2.gif')} width="185px" height="185px"/>
+              <br/>
+              <img src={require('../assets/pets/dog1.gif')} width="200px" height="185px"/>  
               <img src={require('../assets/pets/dog3.gif')} width="400px" height="250px"/>
               <img src={require('../assets/pets/dog4.gif')} width="300px" height="250px"/> 
               <img src={require('../assets/pets/dog2.gif')} width="325px" height="300px"/>
+              <br />
               <img src={require('../assets/pets/hedgehog1.gif')} width="200px" height="200px"/> 
               <img src={require('../assets/pets/hedgehog2.gif')} width="200px" height="200px"/> 
               <img src={require('../assets/pets/hedgehog3.gif')} width="200px" height="200px"/> 
-              </div>
+            </div>
           </div>
         </div>
       </div>
