9ea12758fdc23bf496a29e37e93fe972224be401
Arushi Katyal <katy.arushi@gmail.com>
2022-02-15 20:30:56 -0500

    pets

--- pomo-react/src/routes/Pets.jsx
+++ pomo-react/src/routes/Pets.jsx
@@ -7,7 +7,7 @@ export default function Pets() {
             <h1 className="logo-text text-center"> Choose your PomoPet! </h1>
             <div> 
               <img src={require('../assets/pets/cat1.gif')} width="200px" height="200px"/> 
-              <img src={require('../assets/pets/cat1.gif')} width="200px" height="200px"/> 
+              <img src={'https://c.tenor.com/02ckhzGY8fMAAAAC/pug-dance.gif'} width="200px" height="200px"/> 
               <img src={require('../assets/pets/dog1.gif')} width="200px" height="200px"/> 
               <img src={require('../assets/pets/dog2.gif')} width="200px" height="200px"/> 
               <img src={require('../assets/pets/hedgehog1.gif')} width="200px" height="200px"/> 
