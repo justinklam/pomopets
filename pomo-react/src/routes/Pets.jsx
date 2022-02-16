3e4810e52f31de5603f491b8c3c22615667f2c9a
Rhea <rheaazarraga@gmail.com>
2022-02-15 17:35:04 -0800

    add fun pet pics to pet page

--- pomo-react/src/routes/Pets.jsx
+++ pomo-react/src/routes/Pets.jsx
@@ -5,14 +5,16 @@ export default function Pets() {
         <div className="main-container">
           <div className="text-container">
             <h1 className="logo-text text-center"> Choose your PomoPet! </h1>
-            <div> 
-              <img src={require('../assets/pets/cat1.gif')} width="200px" height="200px"/> 
-              <img src={'https://c.tenor.com/02ckhzGY8fMAAAAC/pug-dance.gif'} width="200px" height="200px"/> 
-              <img src={require('../assets/pets/dog1.gif')} width="200px" height="200px"/> 
-              <img src={require('../assets/pets/dog2.gif')} width="200px" height="200px"/> 
+            <div className="pet-pics text-center"> 
+              <img src={require('../assets/pets/cat1.gif')} width="185px" height="185px"/> 
+              <img src={require('../assets/pets/cat2.gif')} width="185px" height="185px"/> 
+              <img src={require('../assets/pets/dog3.gif')} width="400px" height="250px"/>
+              <img src={require('../assets/pets/pug.gif')} width="300px" height="250px"/> 
+              <img src={require('../assets/pets/dog2.gif')} width="325px" height="300px"/>
               <img src={require('../assets/pets/hedgehog1.gif')} width="200px" height="200px"/> 
               <img src={require('../assets/pets/hedgehog2.gif')} width="200px" height="200px"/> 
-            </div>
+              <img src={require('../assets/pets/hedgehog3.gif')} width="200px" height="200px"/> 
+              </div>
           </div>
         </div>
       </div>
