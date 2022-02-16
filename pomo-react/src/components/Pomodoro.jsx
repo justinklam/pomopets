7bbb224d8a8b796ab8b1cc4f55633d49615fe585
Rhea <rheaazarraga@gmail.com>
2022-02-15 19:00:55 -0800

    install react-circular-progressbar, import it and buildStyles

--- pomo-react/src/components/Pomodoro.jsx
+++ pomo-react/src/components/Pomodoro.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 export default function Pomodoro() {
 
