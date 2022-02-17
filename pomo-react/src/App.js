725588a4f2395a218fb9ee3b72616966ffca6c7e
Justin Lam <justin.kh.lam@gmail.com>
2022-02-16 23:18:34 -0500

    shift index.js back into App.js so that it can call a react function for useState functionality

--- pomo-react/src/App.js
+++ pomo-react/src/App.js
@@ -1,18 +1,45 @@
-// import React from "react";
+import React, {useState} from "react";
+
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import { SessionsContext } from './context/SessionsContext';
+
+// component imports
+import Main from "./components/Main";
+import HomeNavBar from "./components/HomeNavBar";
+
+// route imports
+import Login from "./routes/Login";
+import Pets from "./routes/Pets";
+import Register from "./routes/Register";
+import Statistics from "./routes/Statistics";
+import Timers from "./routes/Timers";
+import About from "./routes/About";
+
+// styles
 // import './styles/App.scss';
 
-// // components
-// import Main from "./components/Main";
-// import MainNavBar from "./components/MainNavBar";
+function App() {
+  const [context, setContext] = useState();
+  return (
+    <BrowserRouter>
+    <SessionsContext.Provider value={[context, setContext]}>
+    <HomeNavBar/>
+    <Routes>
+      <Route path="/" element={<Main title="PomoPets"/>} />
+      <Route path="timers" element={<Timers />} />
+      <Route path="pets" element={<Pets />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+      <Route path="statistics" element={<Statistics />} />
+      <Route path="about" element={<About />} />
+    </Routes>
+    </SessionsContext.Provider>
+  </BrowserRouter>
+  );
+}
 
-// function App() {
-//   return (
-//     <div>
-//       {/* <MainNavBar /> */}
-//       <Main title="PomoPets" />
-//       {/* <Pomodoro /> */}
-//     </div>
-//   );
-// }
-
-// export default App;
+export default App;
