15f2a98752362b5085a63e756751d5374cadad5b
Justin Lam <justin.kh.lam@gmail.com>
2022-02-17 18:09:34 -0500

    refactor code to call a Pets card by mapping through an array

--- pomo-react/src/routes/Pets.jsx
+++ pomo-react/src/routes/Pets.jsx
@@ -1,17 +1,30 @@
+import PetsCard from '../components/PetsCard';
+import pup from "../assets/pets/dog1.gif";
+import hedgehog from "../assets/pets/hedgehog2.gif"
+import cat from "../assets/pets/cat1.gif"
+
 export default function Pets() {
+
+  const pet_info = [{
+    name:"Pup",
+    image: pup
+  }, {
+    name:"Hedgehog",
+    image: hedgehog
+  }, {
+    name:"Cat",
+    image: cat
+  }];
+
   return (
     <>
       <div className="body pt-5">
         <div className="main-container">
           <div className="text-container">
             <h1 className="logo-text text-center"> Choose your PomoPet! </h1>
-            <div> 
-              <img src={require('../assets/pets/cat1.gif')} width="200px" height="200px"/> 
-              <img src={require('../assets/pets/cat1.gif')} width="200px" height="200px"/> 
-              <img src={require('../assets/pets/dog1.gif')} width="200px" height="200px"/> 
-              <img src={require('../assets/pets/dog2.gif')} width="200px" height="200px"/> 
-              <img src={require('../assets/pets/hedgehog1.gif')} width="200px" height="200px"/> 
-              <img src={require('../assets/pets/hedgehog2.gif')} width="200px" height="200px"/> 
+            <div>
+              {pet_info.map((pet) =><PetsCard name={pet.name} image={pet.image}/>
+              )}
             </div>
           </div>
         </div>
