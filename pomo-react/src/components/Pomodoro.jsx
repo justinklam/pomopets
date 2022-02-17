843ef2a35c482f644d3e8d8aa8c3ddffca252df4
Rhea <rheaazarraga@gmail.com>
2022-02-16 18:12:31 -0800

    fix conditional logic to call setMaxSeconds so that the progressbar follows the break countdown, store break session calculation in a variable

--- pomo-react/src/components/Pomodoro.jsx
+++ pomo-react/src/components/Pomodoro.jsx
@@ -4,40 +4,43 @@ import "react-circular-progressbar/dist/styles.css";
 
 export default function Pomodoro(props) {
   const startingTimer = props.startingTimer || 0;
+  console.log("what is it?", startingTimer);
 
   const [displayMessage, setDisplayMessage] = useState(false);
   const [currentSeconds, setCurrentSeconds] = useState(60 * startingTimer);
   const [maxSeconds, setMaxSeconds] = useState(60 * startingTimer);
+
+  const breakTime = (4*60)+59;
 
   useEffect(() => {
     // timer set-up
     let interval = setInterval(() => {
       // NOTE: must clear interval! otherwise bad practice - see what happens without clearing interval
       clearInterval(interval);
-      
+
       if (currentSeconds === 0) {
-        if (displayMessage) {
-          setDisplayMessage(false)
-          setCurrentSeconds(24 * 60)
-        } else {
-          setDisplayMessage(true)
-          setCurrentSeconds(4 * 60 + 59)
+        if (displayMessage) { 
+          setDisplayMessage(false); // hide the break
+          setCurrentSeconds(1 * 60); // reset initial timer
+        } else { // break timer is running
+          setDisplayMessage(true);
+          setCurrentSeconds(breakTime);
+          setMaxSeconds(breakTime);
         }
       } else {
-      setCurrentSeconds(currentSeconds - 1);
-        
+        setCurrentSeconds(currentSeconds - 1); // initial timer normal countdown
       }
     }, 1000);
-  }, [currentSeconds]); 
+  }, [currentSeconds]);
 
   const formatTime = () => {
     let minutes = Math.floor(currentSeconds / 60);
-    minutes = minutes < 10 ? `0${minutes}`: minutes;
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
     let seconds = currentSeconds % 60;
-    seconds = seconds < 10 ? `0${seconds}`: seconds;
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
     return `${minutes}:${seconds}`;
   };
-
+  console.log("-----", {currentSeconds, maxSeconds, value:(currentSeconds / maxSeconds)*100});
   return (
     <div className="pomodoro">
       <div className="message text-center">
