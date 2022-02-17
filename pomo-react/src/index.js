b120bee926d4e547c007a17574c98440aa8c42e3
Justin Lam <justin.kh.lam@gmail.com>
2022-02-16 23:19:37 -0500

    separate code back into App.js for state issues

--- pomo-react/src/index.js
+++ pomo-react/src/index.js
@@ -1,40 +1,13 @@
+import React from 'react';
 import { render } from "react-dom";
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
 
-// component imports
-import Main from "./components/Main";
-import HomeNavBar from "./components/HomeNavBar";
-import Pomodoro from "./components/Pomodoro";
-
-// route imports
-import Login from "./routes/Login";
-import Pets from "./routes/Pets";
-import Register from "./routes/Register";
-import Statistics from "./routes/Statistics";
-import Timers from "./routes/Timers";
-import About from "./routes/About";
+import App from './App';
 
 // styles
 import './styles/App.scss';
 
 const rootElement = document.getElementById("root");
 render(
-  <BrowserRouter>
-    <HomeNavBar/>
-    <Routes>
-      <Route path="/" element={<Main title="PomoPets"/>} />
-      <Route path="timers" element={<Timers />} />
-      <Route path="pets" element={<Pets />} />
-      <Route path="login" element={<Login />} />
-      <Route path="register" element={<Register />} />
-      <Route path="statistics" element={<Statistics />} />
-      <Route path="about" element={<About />} />
-    </Routes>
-    {/* <Pomodoro /> */}
-  </BrowserRouter>,
+  <App/>,
   rootElement
 );
