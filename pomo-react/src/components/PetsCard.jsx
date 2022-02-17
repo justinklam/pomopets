ad904ee07315d0952b41a572a6168b41f42f8f28
Justin Lam <justin.kh.lam@gmail.com>
2022-02-17 18:11:52 -0500

    initialize PetsCard with logic to handle a pet selection click and state

---
+++ pomo-react/src/components/PetsCard.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { Button } from 'react-bootstrap';
+
+
+export default function PetsCard(props) {
+
+  const [toggleName, setToggleName] = useState(false);
+
+  return (
+      <div class="card" style={{width: "18rem"}}>
+        <div class="card-body">
+          <img src={props.image} alt="description" width="200px" height="200px"/> 
+          <h5 class="card-title">{props.name}</h5>
+          <p class="card-text">{props.name} Description</p>
+          <div class="form-group">
+            <Button for="exampleFormControlTextarea1" onClick={() =>setToggleName(!toggleName)}>Choose Your Pet</Button>
+            {toggleName?<div><textarea class="form-control"  placeholder="Name Your Pet" id="exampleFormControlTextarea1" rows="1"></textarea>
+            <Button variant="primary" type="submit">Submit</Button></div>:""}
+          </div>
+        </div>
+      </div>
+  );
+}
