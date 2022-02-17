303b6e4a0713e8d36ad95d2eb59ec7b3d7531217
Justin Lam <justin.kh.lam@gmail.com>
2022-02-16 23:20:46 -0500

    resolve Link issues for react router. ensure state is passed down through useContext. change conditional checks to look for session

--- pomo-react/src/components/HomeNavBar.jsx
+++ pomo-react/src/components/HomeNavBar.jsx
@@ -1,11 +1,15 @@
-import { Link, useLocation } from "react-router-dom";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
 import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import { SessionsContext } from "../context/SessionsContext";
+
 export default function HomeNavBar() {
 
-  let location = useLocation();
+  const [session, setSession] = useContext(SessionsContext)
+  console.log('session', session);
 
   return (
     <Navbar className="main-navbar" bg="light" expand="lg">
@@ -14,19 +18,19 @@ export default function HomeNavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About Pomodoro</Nav.Link>
-            <Nav.Link href="/pets">Pets</Nav.Link>
-            <Nav.Link href="/timers">Timers</Nav.Link>
+            <Link to="/">Home</Link>
+            <Link to="/about">About Pomodoro</Link>
+            <Link to="/pets">Pets</Link>
+            <Link to="/timers">Timers</Link>
             <NavDropdown title="My Account" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
               <NavDropdown.Divider />
-              {!location.state?.email ? '' : <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>}
+              {!session?.state.email ? '' : <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>}
             </NavDropdown>
             <div className="button-div">
-              {location.state?.email ? '' : <Button className="login-button" href="/login" variant="primary">Login</Button>}{' '}
-              {location.state?.email ? '' : <Button className="register-button" href="/register" variant="outline-primary">Register</Button>}{' '}
+              {session?.state.email ? '' : <Button className="login-button" href="/login" variant="primary">Login</Button>}{' '}
+              {session?.state.email ? '' : <Button className="register-button" href="/register" variant="outline-primary">Register</Button>}{' '}
             </div>
           </Nav>
         </Navbar.Collapse>
