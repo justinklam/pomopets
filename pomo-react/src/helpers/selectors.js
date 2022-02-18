138a3fcd6f4fb45e147a0f06b8e4b2ece2cb684b
Justin Lam <justin.kh.lam@gmail.com>
2022-02-18 18:10:31 -0500

    initialize and correct logic for getPets selector function

--- pomo-react/src/helpers/selectors.js
+++ pomo-react/src/helpers/selectors.js
@@ -1,21 +1,18 @@
 const axios = require('axios');
 
-// const getPets = axios.get('/localhost:3030/pets', {
-//   params: {
-//     id: id
-//   }
-// })
-// .then(function (response) {
-//   // handle success
-//   throw response
-// })
-// .catch(function (error) {
-//   // handle error
-//   throw error;
-// })
-// .then(function () {
-//   // always executed
-// });
+const getPets = (userId) =>
+  axios.get('//localhost:3030/pets', {
+  params: {
+    id: userId
+  }
+})
+.then(function (response) {
+  console.log('getPets', response);
+  return response.data;
+})
+.catch(function (error) {
+  throw error;
+});
 
 // const getTimers = axios.get('/localhost:3030/get_timers', {
 //   params: {
@@ -36,6 +33,6 @@ const axios = require('axios');
 
 
 export {
-  // getPets,
+  getPets
   // getTimers
 };
