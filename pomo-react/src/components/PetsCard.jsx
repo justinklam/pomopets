9322f5f10cdea063a94e5bf567bf2e265bc3140d
Arushi Katyal <katy.arushi@gmail.com>
2022-02-18 02:35:37 -0500

    change pets card

--- pomo-react/src/components/PetsCard.jsx
+++ pomo-react/src/components/PetsCard.jsx
@@ -39,15 +39,15 @@ export default function PetsCard(props) {
 
   return (
     <>
-      <div class="card" style={{width: "18rem"}}>
-        <div class="card-body">
+      <div className="card" style={{width: "18rem"}}>
+        <div className="card-body">
           <img src={props.image} alt="description" width="200px" height="200px"/> 
-          <h5 class="card-title">{props.name}</h5>
-          <p class="card-text">{props.name} Description</p>
-          <div class="form-group">
-            <Button for="exampleFormControlTextarea1" onClick={() =>setToggleName(!toggleName)}>Choose Your Pet</Button>
-            {toggleName?<div><textarea class="form-control"  placeholder="Name Your Pet" id="exampleFormControlTextarea1" rows="1" onChange={handleChange}></textarea>
-            <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button></div>:""}
+          <h5 className="card-title">{props.name}</h5>
+          <p className="card-text">{props.description}</p>
+          <div className="form-group">
+            <Button className="pet-button" for="exampleFormControlTextarea1" onClick={() =>setToggleName(!toggleName)}>Choose This Pet</Button>
+            {toggleName?<div><input className="form-control" placeholder="Name Your Pet" id="exampleFormControlTextarea1" rows="1" onChange={handleChange}></input>
+            <Button className="pet-button" type="submit" onClick={handleSubmit}>Submit</Button></div>:""}
           </div>
         </div>
       </div>
--- pomo-react/src/routes/Pets.jsx
+++ pomo-react/src/routes/Pets.jsx
@@ -12,7 +12,7 @@ export default function Pets() {
   }, {
     name:"Hedgehog",
     image: hedgehog,
-    description:"A cuddly creature with mysterious spikes..."
+    description:"A cuddly creature with many spikes!"
   }, {
     name:"Cat",
     image: cat,
@@ -24,25 +24,11 @@ export default function Pets() {
       <div className="body pt-5">
         <div className="main-container">
           <div className="text-container">
-            <h1 className="logo-text text-center"> Choose your PomoPet! </h1>
+            <h1 className="pets logo-text text-center"> Choose your PomoPet! </h1>
 
-            <div>
+            <div className='pets-display'>
               {pet_info.map((pet) => <PetsCard name={pet.name} image={pet.image} description={pet.description}/>
               )}
-
-            {/* <div className="pet-pics text-center"> 
-              <img src={require('../assets/pets/cat1.gif')} alt="pinkpusheen" width="185px" height="185px"/> 
-              <img src={require('../assets/pets/cat2.gif')} alt="bluepusheen" width="185px" height="185px"/>
-              <br/>
-              <img src={require('../assets/pets/dog1.gif')} alt="pugsheen" width="200px" height="185px"/>  
-              <img src={require('../assets/pets/dog3.gif')} alt="toypoodle" width="400px" height="250px"/>
-              <img src={require('../assets/pets/dog4.gif')} alt="downwarddog" width="300px" height="250px"/> 
-              <img src={require('../assets/pets/dog2.gif')} alt="balldog" width="325px" height="300px"/>
-              <br />
-              <img src={require('../assets/pets/hedgehog1.gif')} alt="crownhedgehog" width="200px" height="200px"/> 
-              <img src={require('../assets/pets/hedgehog2.gif')} alt="bowinghedgehog" width="200px" height="200px"/> 
-              <img src={require('../assets/pets/hedgehog3.gif')} alt="spinhedgehog" width="200px" height="200px"/>  */}
-
             </div>
           </div>
         </div>
