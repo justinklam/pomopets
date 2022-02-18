ae6e5a4b8d2f2dd10ea3a1d283538f191b9ce6fd
Arushi Katyal <katy.arushi@gmail.com>
2022-02-18 01:26:54 -0500

    cleanup and remove comments

--- pomo-react/src/components/HomeNavBar.jsx
+++ pomo-react/src/components/HomeNavBar.jsx
@@ -39,30 +39,4 @@ export default function HomeNavBar() {
       </Container>
     </Navbar>
   );
-
-  // return (
-  //   <Navbar className="main-navbar" bg="light" expand="lg">
-  //     <Container>
-  //       <Navbar.Brand href="/">PomoPets</Navbar.Brand>
-  //       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  //       <Navbar.Collapse id="basic-navbar-nav">
-  //         <Nav className="me-auto">
-  //           <Nav.Link href="/">Home</Nav.Link>
-  //           <Nav.Link href="/pets">Pets</Nav.Link>
-  //           <Nav.Link href="/timers">Timers</Nav.Link>
-  //           <Nav.Link href="/about">About Pomodoro</Nav.Link>
-  //           <NavDropdown title="My Account" id="basic-nav-dropdown">
-  //             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-  //             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-  //             <NavDropdown.Divider />
-  //             <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
-  //           </NavDropdown>
-  //           <div className="button-div">
-  //             <Button className="login-button" href="/" variant="primary">Logout</Button>{' '}
-  //           </div>
-  //         </Nav>
-  //       </Navbar.Collapse>
-  //     </Container>
-  //   </Navbar>
-  // );
 };
