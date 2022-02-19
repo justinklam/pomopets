57ca82371d7500c98612939f1959881e2e68cb8f
Justin Lam <justin.kh.lam@gmail.com>
2022-02-19 01:25:54 -0500

    wrap session check inside a conditional so pet card won't render until logged in

--- pomo-react/src/components/MyPet.jsx
+++ pomo-react/src/components/MyPet.jsx
@@ -3,9 +3,9 @@ import { SessionsContext } from "../context/SessionsContext";
 import { getPets } from "../helpers/selectors";
 
 // image assets
-import pup from "../assets/pets/dog1.gif";
-import hedgehog from "../assets/pets/hedgehog2.gif"
-import cat from "../assets/pets/cat1.gif"
+import pup1 from "../assets/pets/dog1.gif";
+import hedgehog2 from "../assets/pets/hedgehog2.gif"
+import cat1 from "../assets/pets/cat1.gif"
 
 export default function MyPet() {
 
@@ -15,44 +15,48 @@ export default function MyPet() {
 
   const pet_info = [{
     name:"Pup",
-    image: pup,
-    description:"A very good boy who love naps!"
+    image: pup1,
   }, {
     name:"Hedgehog",
-    image: hedgehog,
-    description:"A cuddly creature with many spikes!"
+    image: hedgehog2,
   }, {
     name:"Cat",
-    image: cat,
-    description:"A sassy cat who lives in luxury!"
+    image: cat1,
   }];
 
 
   const [pets, setPets] = useState([]);
 
   useEffect(() => {
-  const userPetsJsx = [];
-  const getUserPets = getPets(session.state.id)
-    .then((userPets) => {
-      // console.log('getUserPets-userPets-----', userPets)
-      userPets.map(pet => {
-        let currentPet = {};
-        if (pet.type === 1){
-          currentPet = pet_info[0];
-        }
-
-        // if conditionals for stage and which pet is needed
+    const userPetsJsx = [];
+    if(session.state.id){
+      const getUserPets = getPets(session.state.id)
+        .then((userPets) => {
+          console.log('getUserPets-userPets-----', userPets)
+          userPets.map(pet => {
+            let currentPet = {};
+            if (pet.type === 1){
+              currentPet = pet_info[0];
+            }
+            if (pet.type === 2){
+              currentPet = pet_info[1];
+            }
+            if (pet.type === 3){
+              currentPet = pet_info[2];
+            }
   
-        userPetsJsx.push(<div className="card" style={{width: "18rem"}} key={pet.id}>
-        <div className="card-body">
-          <img src={currentPet.image} alt="description" width="200px" height="200px"/> 
-          <h5 className="card-title">{currentPet.name}</h5>
-          <p className="card-text">{currentPet.description}</p>
-            </div>
-        </div>)
-      });
-      setPets(userPetsJsx);
-    })
+            // if conditionals for stage and which pet is needed
+            userPetsJsx.push(
+            <div className="card" style={{width: "18rem"}} key={pet.id}>
+              <div className="card-body">
+                <img src={currentPet.image} alt="pet-image" width="200px" height="200px"/> 
+                <h5 className="card-title">{currentPet.name}</h5>
+              </div>
+            </div>)
+          });
+          setPets(userPetsJsx);
+        })
+    }
   }, []);
   
   return (
