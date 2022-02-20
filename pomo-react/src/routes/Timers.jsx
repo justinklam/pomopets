45b285ddc3720a7233720103205ba4fe3ca64a88
Rhea <rheaazarraga@gmail.com>
2022-02-20 12:28:43 -0800

    move timer tag dropdown within main container

--- pomo-react/src/routes/Timers.jsx
+++ pomo-react/src/routes/Timers.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Dropdown, DropdownButton } from 'react-bootstrap';
+import { Dropdown, DropdownButton } from "react-bootstrap";
 import Pomodoro from "../components/Pomodoro";
 import MyPet from "../components/MyPet";
 
@@ -10,24 +10,24 @@ export default function Timers() {
 
   const [tag, setTag] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
-  }
+  };
 
   return (
     <>
       <div className="body pt-5">
-        <DropdownButton id="dropdown-basic-button" title="Timer Tag">
-          <Dropdown.Item onClick={handleSubmit}>Study</Dropdown.Item>
-          <Dropdown.Item onClick={handleSubmit}>Exercise</Dropdown.Item>
-          <Dropdown.Item onClick={handleSubmit}>Work</Dropdown.Item>
-          <Dropdown.Item onClick={handleSubmit}>Tidy-Up</Dropdown.Item>
-        </DropdownButton>
         <div className="main-container">
           <div className="text-container pt-2">
+            <DropdownButton id="dropdown-basic-button" title="Timer Tag">
+              <Dropdown.Item onClick={handleSubmit}>Study</Dropdown.Item>
+              <Dropdown.Item onClick={handleSubmit}>Exercise</Dropdown.Item>
+              <Dropdown.Item onClick={handleSubmit}>Work</Dropdown.Item>
+              <Dropdown.Item onClick={handleSubmit}>Tidy-Up</Dropdown.Item>
+            </DropdownButton>
             <h1 className="logo-text text-center"> Timer </h1>
             <Pomodoro startingTimer={timeData.time} />
-            <MyPet/>
+            <MyPet />
           </div>
         </div>
       </div>
