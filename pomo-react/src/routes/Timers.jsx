2b59646952df17e2c940c548c439a923825626c2
Justin Lam <justin.kh.lam@gmail.com>
2022-02-20 18:14:19 -0500

    add Timer tag logic

--- pomo-react/src/routes/Timers.jsx
+++ pomo-react/src/routes/Timers.jsx
@@ -1,18 +1,28 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Dropdown, DropdownButton } from 'react-bootstrap';
 import Pomodoro from "../components/Pomodoro";
 import MyPet from "../components/MyPet";
+import { SessionsContext } from "../context/SessionsContext";
+// import { createTimer } from "../helpers/helpers";
 
 export default function Timers() {
   const timeData = {
-    time: 25,
+    time: 0.05,
   };
 
-  const [tag, setTag] = useState("");
+  const [session, setSession] = useContext(
+    SessionsContext
+  );
+
+  const [tag, setTag] = useState("Misc");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-  }
+    const userId = session.state.id;
+    const description = e.target.text;
+    setTag(description);
+    // createTimer(userId, description)
+  };
 
   return (
     <>
@@ -26,7 +36,7 @@ export default function Timers() {
         <div className="main-container">
           <div className="text-container pt-2">
             <h1 className="logo-text text-center"> Timer </h1>
-            <Pomodoro startingTimer={timeData.time} />
+            <Pomodoro startingTimer={timeData.time} tag={tag} />
             <MyPet/>
           </div>
         </div>
