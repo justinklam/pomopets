0ef2b5f6a399a5e40b6f47db94adce913ab005fd
Justin Lam <justin.kh.lam@gmail.com>
2022-02-20 18:12:31 -0500

    refactor and dry code

--- pomo-react/src/components/MyPet.jsx
+++ pomo-react/src/components/MyPet.jsx
@@ -8,7 +8,7 @@ import pup2 from "../assets/pets/dog2.gif";
 import pup3 from "../assets/pets/dog3.gif";
 import hedgehog1 from "../assets/pets/hedgehog1.gif"
 import hedgehog2 from "../assets/pets/hedgehog2.gif"
-import hedgehog3 from "../assets/pets/hedgehog2.gif"
+import hedgehog3 from "../assets/pets/hedgehog3.gif"
 import cat1 from "../assets/pets/cat1.gif"
 import cat2 from "../assets/pets/cat2.gif"
 import cat3 from "../assets/pets/cat3.gif"
@@ -38,19 +38,19 @@ export default function MyPet() {
   },
   {
     name:"Hedgehog",
-    type: 1,
+    type: 2,
     action: "surprise",
     image: hedgehog1,
   }, 
   {
     name:"Hedgehog",
-    type: 1,
+    type: 2,
     action: "charge",
     image: hedgehog2,
   },
   {
     name:"Hedgehog",
-    type: 1,
+    type: 2,
     action: "jump",
     image: hedgehog3,
   },
@@ -75,43 +75,83 @@ export default function MyPet() {
 
   const [pets, setPets] = useState([]);
 
+  const [petImage, setPetImage] = useState(pet_info[0].image);
+
+  const [userPets, setUserPets] = useState();
+
   useEffect(() => {
     const userPetsJsx = [];
     if(session.state.id){
       const getUserPets = getAllPets(session.state.id)
         .then((userPets) => {
-          console.log('MyPet - getUserPets-alluserPets-----', userPets)
-          userPets.map(pet => {
-            let currentPet = {};
-            // console.log(pet);
+          userPets.forEach ((pet, i) => {
             if (pet.pet.type === 1){
-              currentPet = pet_info[1];
+              userPets[i].pet.info = pet_info[0]
+              userPets[i].pet.info.image2 = pet_info[1].image
+              userPets[i].pet.info.image3 = pet_info[2].image
+              if(pet.pet.stage === 0) {
+                userPets[i].pet.activeImage = pet_info[0].image
+              } else if (pet.pet.stage > 0) {
+                userPets[i].pet.activeImage = pet_info[1].image
+              }
             }
             if (pet.pet.type === 2){
-              currentPet = pet_info[4];
+              userPets[i].pet.info = pet_info[3]
+              userPets[i].pet.info.image2 = pet_info[4].image
+              userPets[i].pet.info.image3 = pet_info[5].image
+              if(pet.pet.stage === 0) {
+                userPets[i].pet.activeImage = pet_info[3].image
+              } else if (pet.pet.stage > 0) {
+                userPets[i].pet.activeImage = pet_info[4].image
+              }
             }
             if (pet.pet.type === 3){
-              currentPet = pet_info[8];
+              userPets[i].pet.info = pet_info[6]
+              userPets[i].pet.info.image2 = pet_info[7].image
+              userPets[i].pet.info.image3 = pet_info[8].image
+              if(pet.pet.stage === 0) {
+                userPets[i].pet.activeImage = pet_info[6].image
+              } else if (pet.pet.stage > 0) {
+                userPets[i].pet.activeImage = pet_info[7].image
+              }
             }
-  
-            // if conditionals for stage and which pet is needed
-            userPetsJsx.push(
-            <div className="card" style={{width: "18rem"}} key={pet.id}>
-              <div className="card-body">
-                <img src={currentPet.image} alt="pet-image" width="200px" height="200px"/> 
-                <h5 className="card-title">{pet.pet.name}</h5>
-              </div>
-            </div>)
-          });
-          setPets(userPetsJsx);
-        })
-    }
-  }, []);
+          }
+        )
+        console.log('foreach Update', userPets);
+      setUserPets(userPets);
+        })}
+      }, []);
+ 
+  const handleClick = function(e){
+    const src = e.target.src
+    // console.log('handleClick', pet, Math.random());
+  };
+
+  // if (e.target.src === window.location.origin+pet_info[0].image){
+  //   // currentPetImage = pet_info[1].image;
+  //   setPetImage(pet_info[1].image);
+  //   console.log('route1')
+  // } else if(e.target.src === window.location.origin+pet_info[1].image){
+  //   // currentPetImage = pet_info[2].image
+  //   setPetImage(pet_info[2].image);
+  //   console.log('route2')
+  // } else if(e.target.src === window.location.origin+pet_info[2].image){
+  //   // currentPetImage = pet_info[0].image
+  //   setPetImage(pet_info[0].image);
+  //   console.log('round3', e.target.src === window.location.origin+pet_info[2].image)                
+  // }
   
   return (
     <>
-    {(pets.length > 0) ? pets.map(x => x) : ""}
+    {userPets ? userPets.map(pet => (
+      <div className="card" style={{width: "18rem"}} key={pet.pet.id}>
+              <div className="card-body">
+                <img src={pet.pet.activeImage} alt="pet-image" width="200px" height="200px" onClick={handleClick}/> 
+                <h5 className="card-title">{pet.pet.name}</h5>
+              </div>
+            </div>
+      )): ""}
     </>
-  );
-
-};
+    
+    // {(pets.length > 0) ? pets.map(x => x) : ""}
+  )};
