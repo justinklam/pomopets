d64cd1e9b5ae36b8e21786ac35f7857df30ca9d8
Justin Lam <justin.kh.lam@gmail.com>
2022-02-19 20:26:28 -0500

    add in logic to import additional art assets. change current pet name to the user's entered name

--- pomo-react/src/components/MyPet.jsx
+++ pomo-react/src/components/MyPet.jsx
@@ -4,8 +4,12 @@ import { getAllPets } from "../helpers/selectors";
 
 // image assets
 import pup1 from "../assets/pets/dog1.gif";
+import pup2 from "../assets/pets/dog2.gif";
+import pup3 from "../assets/pets/dog3.gif";
 import hedgehog2 from "../assets/pets/hedgehog2.gif"
 import cat1 from "../assets/pets/cat1.gif"
+import cat2 from "../assets/pets/cat2.gif"
+import cat3 from "../assets/pets/cat3.gif"
 
 export default function MyPet() {
 
@@ -15,15 +19,45 @@ export default function MyPet() {
 
   const pet_info = [{
     name:"Pup",
+    type: 1,
+    action:"rest",
     image: pup1,
-  }, {
+  },{
+    name:"Pup",
+    type: 1,
+    action:"up",
+    image: pup2,
+  },
+  {
+    name:"Pup",
+    type: 1,
+    action:"party",
+    image: pup3,
+  },
+  {
     name:"Hedgehog",
+    type: 2,
+    action: "faceplant",
     image: hedgehog2,
-  }, {
+  }, 
+  {
     name:"Cat",
+    type: 3,
+    action: "eat",
     image: cat1,
+  },
+  {
+    name:"Cat",
+    type: 3,
+    action: "up",
+    image: cat2,
+  },
+  {
+    name:"Cat",
+    type: 3,
+    action: "float",
+    image: cat3,
   }];
-
 
   const [pets, setPets] = useState([]);
 
@@ -37,13 +71,13 @@ export default function MyPet() {
             let currentPet = {};
             console.log(pet);
             if (pet.pet.type === 1){
-              currentPet = pet_info[0];
-            }
-            if (pet.pet.type === 2){
               currentPet = pet_info[1];
             }
+            if (pet.pet.type === 2){
+              currentPet = pet_info[4];
+            }
             if (pet.pet.type === 3){
-              currentPet = pet_info[2];
+              currentPet = pet_info[6];
             }
   
             // if conditionals for stage and which pet is needed
@@ -51,7 +85,7 @@ export default function MyPet() {
             <div className="card" style={{width: "18rem"}} key={pet.id}>
               <div className="card-body">
                 <img src={currentPet.image} alt="pet-image" width="200px" height="200px"/> 
-                <h5 className="card-title">{currentPet.name}</h5>
+                <h5 className="card-title">{pet.pet.name}</h5>
               </div>
             </div>)
           });
