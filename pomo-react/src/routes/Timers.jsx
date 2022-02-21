843b2733edd57d81365c3de7e48be32bdfc9078d
Arushi Katyal <katy.arushi@gmail.com>
2022-02-20 21:50:58 -0500

    add classanme to container for styling

--- pomo-react/src/routes/Timers.jsx
+++ pomo-react/src/routes/Timers.jsx
@@ -27,7 +27,7 @@ export default function Timers() {
   return (
     <>
       <div className="body pt-5">
-        <div className="main-container">
+        <div className="timer-container main-container">
           <div className="text-container pt-2">
             <h1 className="logo-text text-center"> Timer </h1>
             <Pomodoro startingTimer={timeData.time} tag={tag} />
