c8c2b833157bd839bac1d53c606b996833e1a0ad
Rhea <rheaazarraga@gmail.com>
2022-02-20 16:53:19 -0800

    accept changes to routes path for statistics

--- pomo-react/src/App.js
+++ pomo-react/src/App.js
@@ -23,7 +23,6 @@ function App() {
   const [ context, setContext ] = useState({ state: {} });
   return (
     <BrowserRouter>
-<<<<<<< HEAD
       <SessionsContext.Provider value={[ context, setContext ]}>
         <HomeNavBar />
         <Routes>
@@ -38,22 +37,6 @@ function App() {
         </Routes>
       </SessionsContext.Provider>
     </BrowserRouter>
-=======
-    <SessionsContext.Provider value={[context, setContext]}>
-    <HomeNavBar/>
-    <Routes>
-      <Route path="/" element={<Main title="PomoPets"/>} />
-      <Route path="timers" element={<Timers />} />
-      <Route path="pets" element={<Pets />} />
-      <Route path="login" element={<Login />} />
-      <Route path="register" element={<Register />} />
-      <Route path="statistics" element={<Statistics />} />
-      <Route path="about" element={<About />} />
-      <Route path="logout" element={<Main />} />
-    </Routes>
-    </SessionsContext.Provider>
-  </BrowserRouter>
->>>>>>> c521641d26cb88618809dc2c21d9aa7d2c0d39f8
   );
 }
 
