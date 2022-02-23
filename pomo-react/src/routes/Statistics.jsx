5b56eb8f0135ee6dfd5278567fe98093be730779
Justin Lam <justin.kh.lam@gmail.com>
2022-02-22 22:39:29 -0500

    initialize Statistics.jsx route calling both charts

---
+++ pomo-react/src/routes/Statistics.jsx
@@ -0,0 +1,11 @@
+import LineChart from "../components/LineChart"
+import PieChart from "../components/PieChart"
+
+export default function Statistics() {
+  return(
+    <>
+      <PieChart/>
+      <LineChart/>
+    </>
+  )
+};
